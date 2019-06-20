4d5f07f607c9378c217b26e88f32c46146b354ac
anupbharti <anup.bharti@gmail.com>
2019-06-20 14:43:22 +0000

    Initial version

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,7 +3,7 @@ sap.ui.define([
 		"sap/ui/model/json/JSONModel"
 	], function (BaseController, JSONModel) {
 		"use strict";
-
+ // GitHub Changes
 		return BaseController.extend("fts.cockpit.controller.App", {
 
 			onInit : function () { 
